c1d6d796446fff1bbe85c33999b9a971363bae48
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-06 12:41:01 +0200

    add projects component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,25 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+
+import data from '../../data.js';
+import styles from './projects.module.css';
+import TechnologyUsed from './TechnologyUsed';
+import Links from './Links.jsx';
 
 const Project = () => (
-  <Fragment>
-    <h1 style={{ paddingTop: '25px' }}>Project</h1>
-  </Fragment>
+  <main>
+    <div className={styles.mainContainer}>
+      <h1>Some Things I've built</h1>
+      {data.map((d, key) => (
+        <div className={styles.projectContainer}>
+          <h1 key={key}>{d.name}</h1>
+          <p>{d.description}</p>
+          <img className={styles.image} src={d.projectPhoto} alt="project photo"></img>
+          <TechnologyUsed data={d.technologyUsed}></TechnologyUsed>
+          <Links data={d.link}></Links>
+        </div>
+      ))}
+    </div>
+  </main>
 );
 
 export default Project;
