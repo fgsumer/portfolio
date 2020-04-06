99b8fd2735739562cd77cf549b53752061280e44
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-06 12:33:13 +0200

    add data.js and images of the projects

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    name: 'GitPro',
+    description: 'graduation project',
+    projectPhoto: './images-icons/gitpro.png',
+    technologyUsed: ['NodeJS', 'ReactJS', 'MongoDB'],
+    link: {
+      github: 'https://github.com/fgsumer',
+      live: 'https://www.samanthaming.com/tidbits/76-converting-object-to-array/',
+    },
+  },
+  {
+    name: 'bivakzone',
+    description: 'bivakzone description',
+    projectPhoto: '/images-icons/bivakzone.png',
+    technologyUsed: ['NodeJS', 'ReactJS', 'MongoDB'],
+    link: {
+      github: 'https://github.com/fgsumer',
+      live: 'https://bivakzone.herokuapp.com/',
+    },
+  },
+  {
+    name: 'parallax',
+    description: 'parallax description',
+    projectPhoto: '/images-icons/together.png',
+    technologyUsed: ['html', 'css'],
+    link: {
+      github: 'https://github.com/fgsumer/FontpairswithParallax',
+      live: 'https://fgsumer.github.io/FontpairswithParallax/',
+    },
+  },
+];
+
+export default data;
