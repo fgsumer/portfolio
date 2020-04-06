10a07d13f2c432d18fd446d46d243e943118023b
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-06 12:40:34 +0200

    remove routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-// import NavBar from './Components/NavBar/NavBar';
 import LandingPage from './Components/LandingPage/LandingPage';
-import AboutMe from './Components/AboutMe/AboutMe';
-import Projects from './Components/Projects/Projects';
 
 function App() {
   return (
     <>
-      <Router>
-        {/* <NavBar /> */}
-        <Route exact={true} path="/" component={LandingPage} />
-        <Route exact path="/aboutme" component={AboutMe} />
-        <Route exact path="/projects" component={Projects} />
-      </Router>
+      <LandingPage />
     </>
   );
 }
