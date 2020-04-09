f2c5c31b938f7e1d38f4d9938e5742178ee86e60
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-09 20:53:01 +0200

    update AboutMe Component and add basic styling

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -3,7 +3,28 @@ import styles from './aboutme.module.css';
 
 const AboutMe = () => (
   <div className={styles.container}>
-    <h3>About Me</h3>
+    <h3 style={{ marginTop: '20rem' }}>About Me</h3>
+    <div className={styles.flexContainer}>
+      <div className={styles.flexBox}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A maxime ipsum nihil recusandae
+          corrupti ut minus aliquam, saepe similique, facere earum soluta cupiditate vero! Minima
+          corrupti dicta odit nulla perspiciatis. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. A maxime ipsum nihil recusandae corrupti ut minus aliquam, saepe similique, facere
+          earum soluta cupiditate vero! Minima corrupti dicta odit nulla perspiciatis.
+        </p>
+        <p>
+          I've been working with{' '}
+          <span className={styles.underline}>
+            JavaScript (ES6+), HTML & CSS, React, Node.js, MySQL and MongoDB
+          </span>{' '}
+          recently.
+        </p>
+      </div>
+      <div>
+        <img id="graysacele" className={styles.picture} src="./images-icons/IMG_6288.jpeg" alt="" />
+      </div>
+    </div>
   </div>
 );
 
