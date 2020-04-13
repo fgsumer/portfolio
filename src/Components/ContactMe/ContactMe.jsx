f3c630c653483d2f22e093e76a550a44170c5981
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-13 10:49:56 +0200

    add ContactMe component

---
+++ src/Components/ContactMe/ContactMe.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from './contactme.module.css';
+function ContactMe() {
+  return (
+    <div id="contactme" className={styles.container}>
+      <div className={styles.flexcontainer}>
+        <h1 className={styles.h1}>Get in Touch</h1>
+        <p className={styles.p}>
+          I'm currently looking for opportunities, my inbox is always open whether for hiring me or
+          just to talk. I'll answer your email!
+        </p>
+        <h3 style={{ fontSize: '1.5rem' }}>
+          <a href="mailto:fatmagsumer@gmail.com" className={styles.underline}>
+            Say Hi!
+          </a>
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default ContactMe;
