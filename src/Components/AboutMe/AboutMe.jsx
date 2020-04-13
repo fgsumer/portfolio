641efc809dbbea1c23f5be38cdcb700657eab351
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-13 10:47:56 +0200

    add line-height for the paragraphs

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -6,14 +6,14 @@ const AboutMe = () => (
     <hr className={styles.hrText} data-content="About Me"></hr>
     <div className={styles.flexContainer}>
       <div className={styles.flexBox}>
-        <p>
+        <p style={{ lineHeight: '1.6' }}>
           Hi, my name is Fatma Sümer and I am novice front-end developer who is building a new
           career from the thing I really enjoy. For 8 months, I have honed my skills as a
           web-developer, learning how to build web applications with HTML, CSS, JavaScript, React,
           Node, databasing in MySQL and MongoDB while studying for my master’s degree in Educational
           Studies at KU Leuven.
         </p>
-        <p>
+        <p style={{ lineHeight: '1.6' }}>
           I've loved building things for web and been working with{' '}
           <span className={styles.underline}>
             JavaScript (ES6+), HTML & CSS, React, Node.js, MySQL and MongoDB
