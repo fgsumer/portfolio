5046a3218c45848706728429c1e59b8c95b794dd
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-19 20:31:44 +0200

    add real info to the data.js

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export const data = [
   {
     name: 'GitPro',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      'GitPro is a web application by which especially new developers can create their portfolio with a resume and showcase their best works. It is a full- stack project built in teams of four.',
     projectPhoto: './images-icons/gitpro.png',
-    technologyUsed: ['NodeJS', 'ReactJS', 'MongoDB'],
+    technologyUsed: ['MongoDB', 'Expres', 'ReactJS', 'NodeJS'],
     link: {
-      github: 'https://github.com/fgsumer',
-      live: 'https://www.samanthaming.com/tidbits/76-converting-object-to-array/',
+      github: 'https://github.com/https://github.com/fgsumer/class-5-project-lazyvariable',
+      live: 'https://portfolio-d9052.firebaseapp.com/',
     },
   },
   {
-    name: 'bivakzone',
+    name: 'Locate',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      ' Locate is built for the “Outdoor for all” project that aims to map bivac zones in Belgium (where people can go and pitch their tent legally in nature) on OpenStreetMap and to develop a mobile website for their network.',
     projectPhoto: '/images-icons/bivakzone.png',
-    technologyUsed: ['NodeJS', 'ReactJS', 'MongoDB'],
+    technologyUsed: ['MongoDB', 'NodeJS', 'ReactJS', 'React Leaflet', 'OpenStreetMap'],
     link: {
-      github: 'https://github.com/fgsumer',
+      github: 'https://github.com/HackYourFutureBelgium/BootCamp-Bivakzone',
       live: 'https://bivakzone.herokuapp.com/',
     },
   },
   {
-    name: 'parallax',
+    name: 'Together',
     description:
-      'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      'Together is a showcase of my favourite typeface pairings from Google web fonts by using one of my favourite CSS effects, pure CSS parallax.',
     projectPhoto: '/images-icons/together.png',
-    technologyUsed: ['html', 'css'],
+    technologyUsed: ['HTML', 'CSS'],
     link: {
       github: 'https://github.com/fgsumer/FontpairswithParallax',
       live: 'https://fgsumer.github.io/FontpairswithParallax/',
@@ -35,52 +35,51 @@ export const data = [
 ];
 export const data2 = [
   {
-    name: 'parallax',
+    name: 'Tic Tac Toe Game',
     description:
-      'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    technologyUsed: ['html', 'css'],
+      'It is one of my React practices in which I have built the game tic-tac-toe in React, using React hooks.',
+    technologyUsed: ['React'],
     link: {
-      github: 'https://github.com/fgsumer/FontpairswithParallax',
-      live: 'https://fgsumer.github.io/FontpairswithParallax/',
+      github: 'https://github.com/fgsumer/tic-tac-toe',
     },
   },
   {
-    name: 'tictactoe',
+    name: 'JavaScript30',
     description:
-      'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    technologyUsed: ['html', 'css'],
+      "It's a collection of exercises to get inside JS built by following his 30 Day Vanilla JS Coding Challenge but with my own twist and tweaks of course. Live demos are on my codepen account.",
+    technologyUsed: ['HTML', 'CSS', 'JavaScript'],
     link: {
-      github: 'https://github.com/fgsumer/FontpairswithParallax',
-      live: 'https://fgsumer.github.io/FontpairswithParallax/',
+      github: 'https://github.com/fgsumer/JavaScript30',
+      live: 'https://codepen.io/fsumer',
     },
   },
-  {
-    name: 'parallax',
-    description:
-      'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    technologyUsed: ['html', 'css'],
-    link: {
-      github: 'https://github.com/fgsumer/FontpairswithParallax',
-      live: 'https://fgsumer.github.io/FontpairswithParallax/',
-    },
-  },
-  {
-    name: 'tictactoe',
-    description:
-      'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    technologyUsed: ['html', 'css'],
-    link: {
-      github: 'https://github.com/fgsumer/FontpairswithParallax',
-      live: 'https://fgsumer.github.io/FontpairswithParallax/',
-    },
-  },
-  {
-    name: 'parallax',
-    description:
-      'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    technologyUsed: ['html', 'css'],
-    link: {
-      github: 'https://github.com/fgsumer/FontpairswithParallax',
-    },
-  },
+  // {
+  //   name: 'parallax',
+  //   description:
+  //     'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+  //   technologyUsed: ['html', 'css'],
+  //   link: {
+  //     github: 'https://github.com/fgsumer/FontpairswithParallax',
+  //     live: 'https://fgsumer.github.io/FontpairswithParallax/',
+  //   },
+  // },
+  // {
+  //   name: 'tictactoe',
+  //   description:
+  //     'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+  //   technologyUsed: ['html', 'css'],
+  //   link: {
+  //     github: 'https://github.com/fgsumer/FontpairswithParallax',
+  //     live: 'https://fgsumer.github.io/FontpairswithParallax/',
+  //   },
+  // },
+  // {
+  //   name: 'parallax',
+  //   description:
+  //     'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+  //   technologyUsed: ['html', 'css'],
+  //   link: {
+  //     github: 'https://github.com/fgsumer/FontpairswithParallax',
+  //   },
+  // },
 ];
