90494fa431be6b5d86332018aef22a1fdc81d624
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-04-19 20:31:06 +0200

    add target="_blank" to the links

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,17 +6,36 @@ function Footer() {
     <>
       <div className={styles.footerContainer}>
         <div className={styles.icons}>
-          <a href="https://www.linkedin.com/in/fatmas%C3%BCmer/">
+          <a
+            href="https://www.linkedin.com/in/fatmas%C3%BCmer/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img className={styles.icon} src="/images-icons/linkedin.svg" alt="linkedin icon" />
           </a>
-          <a href="https://github.com/fgsumer">
-            <img className={styles.icon} src="/images-icons/github.svg" alt="github icon" />
+          <a href="https://github.com/fgsumer" target="_blank">
+            <img
+              className={styles.icon}
+              src="/images-icons/github.svg"
+              alt="github icon"
+              rel="noopener noreferrer"
+            />
           </a>
-          <a href="https://twitter.com/fatmaagocen">
-            <img className={styles.icon} src="/images-icons/twitter.png" alt="twitter icon"></img>
+          <a href="https://twitter.com/fatmaagocen" target="_blank">
+            <img
+              className={styles.icon}
+              src="/images-icons/twitter.png"
+              alt="twitter icon"
+              rel="noopener noreferrer"
+            />
           </a>
-          <a href="https://https://codepen.io/fsumer">
-            <img className={styles.icon} src="/images-icons/codepen.svg" alt="twitter icon"></img>
+          <a href="https://https://codepen.io/fsumer" target="_blank">
+            <img
+              className={styles.icon}
+              src="/images-icons/codepen.svg"
+              alt="twitter icon"
+              rel="noopener noreferrer"
+            />
           </a>
         </div>
         <p>Designed & Built by Fatma Sümer ©2020 </p>
--- src/Components/NavBar/VerticalNavBar.jsx
+++ src/Components/NavBar/VerticalNavBar.jsx
@@ -5,16 +5,20 @@ import styles from './verticalnavbar.module.css';
 function VerticalNavBar() {
   return (
     <div className={styles.container1}>
-      <a href="https://www.linkedin.com/in/fatmas%C3%BCmer/">
+      <a
+        href="https://www.linkedin.com/in/fatmas%C3%BCmer/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img className={styles.icon1} src="/images-icons/linkedin.svg" alt="linkedin icon" />
       </a>
-      <a href="https://github.com/fgsumer">
+      <a href="https://github.com/fgsumer" target="_blank" rel="noopener noreferrer">
         <img className={styles.icon2} src="/images-icons/github.svg" alt="github icon" />
       </a>
-      <a href="https://twitter.com/fatmaagocen">
+      <a href="https://twitter.com/fatmaagocen" target="_blank" rel="noopener noreferrer">
         <img className={styles.icon3} src="/images-icons/twitter.png" alt="twitter icon"></img>
       </a>
-      <a href="https://https://codepen.io/fsumer">
+      <a href="https://codepen.io/fsumer" target="_blank" rel="noopener noreferrer">
         <img className={styles.icon4} src="/images-icons/codepen.svg" alt="twitter icon"></img>
       </a>
       <div id={styles.verticalline}></div>
