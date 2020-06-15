81f95914b973d782475c746302eecf0fc637e288
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-06-15 12:41:29 +0200

    update links & replace <br>'s with <p> at about me section

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -6,15 +6,18 @@ const AboutMe = () => (
     <hr className={styles.hrText} data-content="About Me"></hr>
     <div className={styles.flexContainer}>
       <div className={styles.flexBox}>
-        <p style={{ lineHeight: '1.6' }}>
-          Hi, my name is Fatma Sümer, nice to meet you! <br />
-          For a year, I have honed my skills as a web developer, learning how to build web
-          applications, both front-end and back-end. In addition to web development, I am completing
-          my master’s degree in Educational Studies at KU Leuven. <br />I love building things for
-          the web and working with{' '}
-          <b>JavaScript (ES6+), HTML & CSS, React, Node.js, MySQL and MongoDB </b>
-          recently. I work well in a team, having collaborated with junior and senior developers in
-          a range of projects.
+        <p style={{ lineHeight: '1.3' }}>
+          Hi, my name is Fatma Sümer, nice to meet you! For a year, I have honed my skills as a web
+          developer, learning how to build web applications, both front-end and back-end.
+        </p>
+        <p style={{ lineHeight: '1.3' }}>
+          In addition to web development, I am completing my master’s degree in Educational Studies
+          at KU Leuven. I love building things for the web and working with{' '}
+          <b>JavaScript (ES6+), HTML & CSS, React, Node.js, MySQL and MongoDB</b> recently.
+        </p>{' '}
+        <p style={{ lineHeight: '1.3' }}>
+          I work well in a team, having collaborated with junior and senior developers in a range of
+          projects.
         </p>
       </div>
       <img id="graysacele" className={styles.picture} src="./images-icons/IMG_6288.jpeg" alt="" />
--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const data = [
     projectPhoto: './images-icons/gitpro.png',
     technologyUsed: ['MongoDB', 'React', 'NodeJS', 'CSS'],
     link: {
-      github: 'https://github.com/fgsumer/GitPro',
+      github: 'https://github.com/fgsumer/gitpro',
       live: 'https://portfolio-d9052.firebaseapp.com/',
     },
   },
@@ -17,7 +17,7 @@ export const data = [
     projectPhoto: '/images-icons/bivakzone.png',
     technologyUsed: ['MongoDB', 'NodeJS', 'React', 'React Leaflet', 'OpenStreetMap'],
     link: {
-      github: 'https://github.com/fgsumer/Locate',
+      github: 'https://github.com/fgsumer/locate',
       live: 'https://bivakzone.herokuapp.com/',
     },
   },
@@ -39,7 +39,7 @@ export const data2 = [
       'Part of the Complete JavaScript Course I completed, this project put JavaScript concepts from the section on DOM manipulation and events into practice.',
     technologyUsed: ['JavaScript', 'HTML', 'CSS'],
     link: {
-      github: 'https://github.com/fgsumer/Pig-Game',
+      github: 'https://github.com/fgsumer/pig-game',
     },
   },
   {
@@ -57,7 +57,7 @@ export const data2 = [
       "It's a collection of exercises to get inside JS built by following his 30 Day Vanilla JS Coding Challenge but with my own twist and tweaks of course. Live demos are on my codepen account.",
     technologyUsed: ['HTML', 'CSS', 'JavaScript'],
     link: {
-      github: 'https://github.com/fgsumer/JavaScript30',
+      github: 'https://github.com/fgsumer/javascript30',
       live: 'https://codepen.io/fsumer',
     },
   },
@@ -68,7 +68,7 @@ export const data2 = [
       'Together is a showcase of my favourite typeface pairings from Google web fonts by using one of my favourite CSS effects, pure CSS parallax.',
     technologyUsed: ['HTML', 'CSS'],
     link: {
-      github: 'https://github.com/fgsumer/FontpairswithParallax',
+      github: 'https://github.com/fgsumer/fontpairswithparallax',
       live: 'https://fgsumer.github.io/FontpairswithParallax/',
     },
   },
